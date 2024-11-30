35a862da23ed8b2fa5fff100a89054662051d97d
Ivan Mikushin <imikushin@users.noreply.github.com>
2024-11-30 13:47:00 -0800

    migrate toad-token to SP1: finish up the contract code

--- examples/toad-token/src/lib.rs
+++ examples/toad-token/src/lib.rs
@@ -1,27 +1,65 @@
 use charms_data::{
-    token_amounts_balanced, AppId, Charm, Transaction, UtxoId, NFT, TOKEN,
+    nft_state_preserved, token_amounts_balanced, AppId, Charm, Data, Transaction, UtxoId, NFT,
+    TOKEN,
 };
 
 pub fn main() {
-    todo!()
+    let (app_id, tx, x, w): (AppId, Transaction, Data, Data) = sp1_zkvm::io::read();
+    assert_eq!(x, Data::empty());
+    assert_eq!(w, Data::empty());
+    assert!(satisfies_contract(&app_id, &tx));
+    sp1_zkvm::io::commit(&(&app_id, &tx, &x));
 }
 
-pub fn toad_token_policy(self_app_id: AppId, tx: Transaction, _x: (), _w: ()) -> bool {
-    assert_eq!(self_app_id.tag, TOKEN);
-    assert!(token_amounts_balanced(&self_app_id, &tx) || can_mint_or_burn(&self_app_id, &tx));
+pub fn satisfies_contract(app_id: &AppId, tx: &Transaction) -> bool {
+    match app_id.tag {
+        NFT => {
+            assert!(nft_contract_satisfied(app_id, tx))
+        }
+        TOKEN => {
+            assert!(token_contract_satisfied(app_id, tx))
+        }
+        _ => unreachable!(),
+    }
     true
 }
 
-fn can_mint_or_burn(self_app_id: &AppId, tx: &Transaction) -> bool {
-    let minting_utxo_id = Some(self_app_id.clone().id);
+fn nft_contract_satisfied(app_id: &AppId, tx: &Transaction) -> bool {
+    assert!(nft_state_preserved(app_id, tx) || can_mint_nft(app_id, tx));
+    true
+}
 
-    // see if the transaction has an input with utxo_id == the token's
-    // app_id.prefix or if it involves an NFT with app_id.prefix == the
-    // token's app_id.prefix
+fn can_mint_nft(self_app_id: &AppId, tx: &Transaction) -> bool {
+    // can only mint an NFT with this contract if spending a UTXO with the same ID.
+    assert!(tx
+        .ins
+        .iter()
+        .any(|utxo| utxo.id == Some(self_app_id.id.clone())));
+    // can mint no more than one NFT.
+    assert_eq!(
+        tx.outs
+            .iter()
+            .filter(|&utxo| utxo
+                .charm
+                .iter()
+                .any(|(app_id, _)| app_id.id == self_app_id.id))
+            .count(),
+        1
+    );
+    true
+}
+
+fn token_contract_satisfied(app_id: &AppId, tx: &Transaction) -> bool {
+    assert!(token_amounts_balanced(app_id, tx) || can_mint_token(app_id, tx));
+    true
+}
+
+fn can_mint_token(app_id: &AppId, tx: &Transaction) -> bool {
+    // see if there's any input with an NFT with app_id.id == the token's app_id.id
     if tx
         .ins
         .iter()
-        .any(|utxo| charm_has_nft_with_app_id_prefix(&utxo.charm, &self_app_id.id))
+        .any(|utxo| charm_has_managing_nft(&utxo.charm, &app_id.id))
     {
         return true;
     }
@@ -29,10 +67,10 @@ fn can_mint_or_burn(self_app_id: &AppId, tx: &Transaction) -> bool {
     false
 }
 
-fn charm_has_nft_with_app_id_prefix(charm: &Charm, nft_app_id_id: &UtxoId) -> bool {
+fn charm_has_managing_nft(charm: &Charm, nft_id: &UtxoId) -> bool {
     charm
         .iter()
-        .any(|(app_id, _)| app_id.tag == NFT && nft_app_id_id == &app_id.id)
+        .any(|(app_id, _)| app_id.tag == NFT && &app_id.id == nft_id)
 }
 
 #[cfg(test)]
