83a786a4eb7af90ede052210a6e8cfe5d95adcd4
Ivan Mikushin <imikushin@users.noreply.github.com>
2024-12-12 17:52:45 -0800

    Use spell-checker as the validator binary name

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@ mod script;
 pub mod spell;
 pub mod tx;
 
-pub const SPELL_CHECKER_BINARY: &[u8] =
-    include_bytes!("../spell-checker/elf/riscv32im-succinct-zkvm-elf");
+pub const SPELL_CHECKER_BINARY: &[u8] = include_bytes!(
+    "../spell-checker/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/spell-checker"
+);
 
 pub static SPELL_VK: LazyLock<String> = LazyLock::new(|| {
     let client = ProverClient::new();
