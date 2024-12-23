9b614c21560d6892790746694d68da9d3cb0182f
Ivan Mikushin <imikushin@users.noreply.github.com>
2024-12-22 17:01:31 -0800

    `charms app build`
    Builds the app in the current dir.

--- src/commands/app.rs
+++ src/commands/app.rs
@@ -1,6 +1,18 @@
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use charms::app;
 use std::fs;
+
+pub fn build() -> Result<()> {
+    let mut child = std::process::Command::new("cargo")
+        .args(&["prove", "build"])
+        .stdout(std::process::Stdio::piped())
+        .spawn()?;
+    let stdout = child.stdout.take().expect("Failed to open stdout");
+    std::io::copy(&mut std::io::BufReader::new(stdout), &mut std::io::stderr())?;
+    let status = child.wait()?;
+    ensure!(status.success());
+    Ok(())
+}
 
 pub fn vk(path: Option<String>) -> Result<()> {
     let prover = app::Prover::new();
@@ -8,14 +20,7 @@ pub fn vk(path: Option<String>) -> Result<()> {
     let binary = match path {
         Some(path) => fs::read(path)?,
         None => {
-            let mut child = std::process::Command::new("cargo")
-                .args(&["prove", "build"])
-                .stdout(std::process::Stdio::piped())
-                .spawn()?;
-            let stdout = child.stdout.take().expect("Failed to open stdout");
-            std::io::copy(&mut std::io::BufReader::new(stdout), &mut std::io::stderr())?;
-            let status = child.wait()?;
-            assert!(status.success());
+            build()?;
             fs::read("./elf/riscv32im-succinct-zkvm-elf")?
         }
     };
