3d6d006ef734778916c66c6870fd62d5a2738344
Ivan Mikushin <imikushin@users.noreply.github.com>
2025-01-18 15:15:03 -0800

    version=0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod utils;
 
 pub const SPELL_CHECKER_BINARY: &[u8] = include_bytes!("./bin/charms-spell-checker");
 
-pub const SPELL_VK: &str = "0x0094c6013afd4d6ee47722ab5c557f695399b451d71b4495ea58165cb12864d6";
+pub const SPELL_VK: &str = "0x00e6516c2f233068f4480c51d1dfbb45da64cd2e0ba1a058d65e7f64de1a8f4f";
 
 #[cfg(test)]
 mod test {
@@ -20,6 +20,6 @@ mod test {
         let (_, vk) = client.setup(SPELL_CHECKER_BINARY);
         let s = vk.bytes32();
 
-        assert_eq!(SPELL_VK, s.as_str(),);
+        assert_eq!(SPELL_VK, s.as_str());
     }
 }
